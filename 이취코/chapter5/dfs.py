983b3105de7cb50d9f98aa4009cfaf54aaff806d
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-06-30 17:22:26 +0900

    [DFS] 인접행렬과 재귀함수 이용

---
+++ 이취코/chapter5/dfs.py
@@ -0,0 +1,30 @@
+#  3 ------  1 - 2 - 7
+#  ㅣ \         \   /  \
+#  4 - 5         8     6
+
+def	dfs(graph, visited, v) :
+	visited[v] = True
+	print(v, end=' ')
+	for i in graph[v] :
+		if visited[i] == False :
+			dfs(graph, visited, i)
+
+# 주어진 그래프
+graph = [
+	[],
+	[2, 3, 8],
+	[1, 7],
+	[1, 4, 5],
+	[3, 5],
+	[3, 4],
+	[7],
+	[2, 6, 8],
+	[1, 7]
+]
+
+# 방문한 노드인지 확인
+visited = [False] * 9
+
+# 시작 노드
+start = 1
+dfs(graph, visited, start)
