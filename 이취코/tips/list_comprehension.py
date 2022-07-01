ff4cb01629bc2df93d771e4ea0b198cad94838e6
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-01 14:57:32 +0900

    [문법] 리스트 컴프리헨션 활용법

--- 이취코/tips/list_comprehension.py
+++ 이취코/tips/list_comprehension.py
@@ -7,3 +7,10 @@ N = 3
 M = 4
 array = [[0] * M for _ in range(N)]
 print(array)
+
+# 특정한 값의 원소를 리스트에서 모두 제거하기
+# 기존 리스트에서 제거를 하는 것이 아니라, 그것을 제외한 새로운 리스트를 만들어 반환!
+array = [1, 2, 3, 4, 5, 5, 5]
+remove_set = {3, 5}
+result = [x for x in array if x not in remove_set]
+print(result)
