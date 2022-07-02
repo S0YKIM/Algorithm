80284b6deac49be35811acfa0af927b9ef08ae09
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-07-02 12:08:34 +0900

    [실전문제3] 음료수 얼려먹기 프로그래머스 형식으로 풀이

---
+++ 이취코/chapter5/음료수얼려먹기.py
@@ -0,0 +1,37 @@
+N = 4
+M = 5
+array = [
+	[0, 0, 1, 1, 0],
+	[0, 0, 0, 1, 1],
+	[1, 1, 1, 1, 1],
+	[0, 0, 0, 0, 0]
+]
+
+
+def	dfs(N, M, tray, x, y) :
+	if x < 0 or x >= N or y < 0 or y >= M :
+		return False
+	if tray[x][y] == 0 :
+		tray[x][y] = 1
+		dfs(N, M, tray, x-1, y)
+		dfs(N, M, tray, x+1, y)
+		dfs(N, M, tray, x, y-1)
+		dfs(N, M, tray, x, y+1)
+		return True
+	return False
+
+
+def	solution(N, M, array) :
+	answer = 0
+	tray = list()
+	for i in range(N) :
+		tray.append(list())
+		for j in range(M) :
+			tray[i].append(array[i][j])
+	for i in range(N) :
+		for j in range(M) :
+			if dfs(N, M, tray, i, j) :
+				answer += 1
+	return answer
+
+print(solution(N, M, array))
